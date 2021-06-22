08f890cde1a319a7ff43be25c427c6635b28308b
nupur-jeswani <jeswaninupur@gmail.com>
2021-06-22 21:11:07 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Intro from './Components/Intro';
 
 function App() {
